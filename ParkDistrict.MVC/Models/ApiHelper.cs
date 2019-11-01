dd3c82ff3af6d3a7da30a271681257b3f43496ca
Micah Zaayer <micahzaayer@gmail.com>
2019-11-01 12:57:39 -0700

    add views to mvc and park list to index.

---
+++ ParkDistrict.MVC/Models/ApiHelper.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace ParkDistrict.Models
+{
+    class ApiHelper
+    {
+        public static async Task<string> ApiCallIndex()
+        {
+            RestClient client = new RestClient("http://localhost:5000/api/parks");
+            RestRequest request = new RestRequest(Method.GET);
+            var response = await client.ExecuteTaskAsync(request);
+            return response.Content;
+        }
+
+        public static async Task<string> ApiCallIndexSearch(string search)
+        {
+            RestClient client = new RestClient("http://localhost:5000/api/parks");
+            RestRequest request = new RestRequest(Method.GET);
+            request.AddParameter("flex", search);
+            Console.WriteLine("search = ", search);
+            var response = await client.ExecuteTaskAsync(request);
+            return response.Content;
+        }
+
+        public static async Task<string> ApiCallDetails(int id)
+        {
+            RestClient client = new RestClient("http://localhost:5000");
+            RestRequest request = new RestRequest("/api/parks/{id}", Method.GET);
+            request.AddUrlSegment("id", id);
+            var response = await client.ExecuteTaskAsync(request);
+            return response.Content;
+        }
+
+        public static async Task<string> ApiCallCreate(Park park)
+        {
+            RestClient client = new RestClient("http://localhost:5000");
+            RestRequest request = new RestRequest("api/parks", Method.POST);
+            request.AddJsonBody(park);
+            var response = await client.ExecuteTaskAsync(request);
+            return response.Content;
+        }
+
+        public static async Task<string> ApiCallDelete(int id)
+        {
+            RestClient client = new RestClient("http://localhost:5000");
+            RestRequest request = new RestRequest("api/parks/{id}", Method.DELETE);
+            request.AddUrlSegment("id", id);
+            var response = await client.ExecuteTaskAsync(request);
+            return response.Content;
+        }
+
+        public static async Task<string> ApiCallEdit(Park park)
+        {
+            RestClient client = new RestClient("http://localhost:5000");
+            RestRequest request = new RestRequest("api/parks/{id}", Method.PUT);
+            request.AddUrlSegment("id", park.ParkId);
+            request.AddJsonBody(park);
+            var response = await client.ExecuteTaskAsync(request);
+            return response.Content;
+        }
+    }
+}
--- ParkDistrict.MVC/Models/Park.cs
+++ ParkDistrict.MVC/Models/Park.cs
@@ -14,5 +14,57 @@ namespace ParkDistrict.Models
         public string Location { get; set; }
         public bool Playground { get; set; }
         public bool PicnicArea { get; set; }
+
+        public static List<Park> ParkListSearch(string search)
+        {
+            List<Park> park;
+            if (string.IsNullOrEmpty(search))
+            {
+                var apiCallTask = ApiHelper.ApiCallIndex();
+                var result = apiCallTask.Result;
+
+                JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result);
+                park = JsonConvert.DeserializeObject<List<Park>>(jsonResponse.ToString());
+            }
+            else
+            {
+                var apiCallTask = ApiHelper.ApiCallIndexSearch(search);
+                var result = apiCallTask.Result;
+
+                JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result);
+                park = JsonConvert.DeserializeObject<List<Park>>(jsonResponse.ToString());
+            }
+            return park;
+        }
+
+        public static Park ParkDetails(int id)
+        {
+            var apiCallTask = ApiHelper.ApiCallDetails(id);
+            var result = apiCallTask.Result;
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+            Park mushroom = JsonConvert.DeserializeObject<Park>(jsonResponse.ToString());
+            Console.WriteLine(mushroom);
+            return mushroom;
+        }
+
+        public async Task<int> ParkCreate()
+        {
+            var result = await ApiHelper.ApiCallCreate(this);
+            return 0;
+        }
+
+        public async static Task<int> ParkDelete(int id)
+        {
+            Console.WriteLine($"mushroom delete id = {id}");
+            var result = await ApiHelper.ApiCallDelete(id);
+            return 0;
+        }
+
+        public async static Task<int> ParkEdit(Park mushroom)
+        {
+            var result = await ApiHelper.ApiCallEdit(mushroom);
+            return 0;
+        }
+
     }
 }
